a10fae7d169d3f8bd7ee861bb4cd5385b3fe8394
Alex Xiong <awx1@rice.edu>
2020-05-18 09:49:40 -0400

    New student link finished

--- app/admin/views.py
+++ app/admin/views.py
@@ -4,9 +4,9 @@ from flask import abort, flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from . import admin
-from forms import DepartmentForm, EmployeeAssignForm, RoleForm
+from forms import DepartmentForm, RoleForm, FreshmanForm
 from .. import db
-from ..models import Department, Employee, Role
+from ..models import Department, Employee, Freshman
 
 
 def check_admin():
@@ -205,6 +205,98 @@ def delete_role(id):
 
     return render_template(title="Delete Role")
 
+@admin.route('/freshmans', methods=['GET', 'POST'])
+@login_required
+def list_freshmans():
+    """
+    List all departments
+    """
+    check_admin()
+
+    freshmans = Freshman.query.all()
+
+    return render_template('admin/freshmans/freshmans.html',
+                           freshman=freshmans, title="Freshmans")
+
+
+@admin.route('/freshmans/add', methods=['GET', 'POST'])
+@login_required
+def add_freshman():
+    """
+    Add a department to the database
+    """
+    check_admin()
+
+    add_freshman = True
+
+    form = FreshmanForm()
+    if form.validate_on_submit():
+        freshman = Freshman(name=form.name.data,
+                                netID=form.netID.data)
+        try:
+            # add department to the database
+            db.session.add(freshman)
+            db.session.commit()
+            flash('You have successfully added a new freshman.')
+        except:
+            # in case department name already exists
+            flash('Error: freshman name already exists.')
+
+        # redirect to departments page
+        return redirect(url_for('admin.list_freshmans'))
+
+    # load department template
+    return render_template('admin/freshmans/freshman.html', action="Add",
+                           add_freshman=add_freshman, form=form,
+                           title="Add Freshman")
+
+
+@admin.route('/freshmans/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_freshman(id):
+    """
+    Edit a department
+    """
+    check_admin()
+
+    add_freshman = False
+
+    freshman = Freshman.query.get_or_404(id)
+    form = FreshmanForm(obj=freshman)
+    if form.validate_on_submit():
+        freshman.name = form.name.data
+        freshman.description = form.description.data
+        db.session.commit()
+        flash('You have successfully edited the freshman.')
+
+        # redirect to the departments page
+        return redirect(url_for('admin.list_freshmans'))
+
+    form.description.data = freshman.description
+    form.name.data = freshman.name
+    return render_template('admin/freshmans/freshman.html', action="Edit",
+                           add_freshman=add_freshman, form=form,
+                           freshman=freshman, title="Edit Freshman")
+
+
+@admin.route('/freshmans/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_freshman(id):
+    """
+    Delete a department from the database
+    """
+    check_admin()
+
+    freshman = Freshman.query.get_or_404(id)
+    db.session.delete(freshman)
+    db.session.commit()
+    flash('You have successfully deleted the freshman.')
+
+    # redirect to the departments page
+    return redirect(url_for('admin.list_freshman'))
+
+    return render_template(title="Delete freshman")
+
 @admin.route('/employees')
 @login_required
 def list_employees():
@@ -218,33 +310,33 @@ def list_employees():
                            employees=employees, title='Employees')
 
 
-@admin.route('/employees/assign/<int:id>', methods=['GET', 'POST'])
-@login_required
-def assign_employee(id):
-    """
-    Assign a department and a role to an employee
-    """
-    check_admin()
+# @admin.route('/employees/assign/<int:id>', methods=['GET', 'POST'])
+# @login_required
+# def assign_employee(id):
+#     """
+#     Assign a department and a role to an employee
+#     """
+#     check_admin()
 
-    employee = Employee.query.get_or_404(id)
+#     employee = Employee.query.get_or_404(id)
 
-    # prevent admin from being assigned a department or role
-    if employee.is_admin:
-        abort(403)
+#     # prevent admin from being assigned a department or role
+#     if employee.is_admin:
+#         abort(403)
 
-    form = EmployeeAssignForm(obj=employee)
-    if form.validate_on_submit():
-        employee.department = form.department.data
-        employee.role = form.role.data
-        db.session.add(employee)
-        db.session.commit()
-        flash('You have successfully assigned a department and role.')
+#     form = EmployeeAssignForm(obj=employee)
+#     if form.validate_on_submit():
+#         employee.department = form.department.data
+#         employee.role = form.role.data
+#         db.session.add(employee)
+#         db.session.commit()
+#         flash('You have successfully assigned a department and role.')
 
-        # redirect to the roles page
-        return redirect(url_for('admin.list_employees'))
+#         # redirect to the roles page
+#         return redirect(url_for('admin.list_employees'))
 
-    return render_template('admin/employees/employee.html',
-                           employee=employee, form=form,
-                           title='Assign Employee')
+#     return render_template('admin/employees/employee.html',
+#                            employee=employee, form=form,
+#                            title='Assign Employee')
 
 
