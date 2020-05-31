a2151de588447d5f4d2dddcd01964f6aa7e153a9
Alex Xiong <awx1@rice.edu>
2020-05-31 10:24:46 -0400

    stuff

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CsrfProtect
 
 # local imports
 from config import app_config
@@ -25,7 +26,12 @@ def create_app(config_name):
         app.config.from_object(app_config[config_name])
         app.config.from_pyfile('config.py')
 
+    # app = Flask(__name__, instance_relative_config=True)
+    # app.config.from_object(app_config[config_name])
+    # app.config.from_pyfile('config.py')
+
     Bootstrap(app)
+    CsrfProtect(app)
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
